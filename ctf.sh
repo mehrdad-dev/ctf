29f99935b4894a01ddd8d9e69af9ab9c3cd8849e
mehrdad-dev <40211374+mehrdad-dev@users.noreply.github.com>
2019-06-03 22:19:34 +0430

    Add files via upload

---
+++ ctf.sh
@@ -0,0 +1,14 @@
+
+#!/bin/bash
+
+echo "welcome mehrdad.dev :)"
+
+read -p "enter password : " pass
+if [ pass == "wintall_tor98" ]; then
+echo "installing ..."
+apt-get install tor
+sleep 2
+echo "run ..."
+tor
+sleep 1
+echo "done !"
