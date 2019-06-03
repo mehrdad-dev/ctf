48420d7a25db3a31752cf722f9621946594923d5
mehrdad-dev <40211374+mehrdad-dev@users.noreply.github.com>
2019-06-03 22:26:13 +0430

    Update ctf.sh

--- ctf.sh
+++ ctf.sh
@@ -4,11 +4,15 @@
 echo "welcome mehrdad.dev :)"
 
 read -p "enter password : " pass
-if [ pass == "wintall_tor98" ]; then
-echo "installing ..."
-apt-get install tor
-sleep 2
-echo "run ..."
-tor
-sleep 1
-echo "done !"
+if [ pass == "wintall_tor98" ]
+then
+    echo "installing ..."
+    apt-get install tor
+    sleep 2
+    echo "run ..."
+    tor
+    sleep 1
+    echo "done !"
+else
+    echo "ban ip !"
+fi
